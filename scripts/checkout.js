c80d025c2896372a934ef3eff616b430a1a769ee
SankaleDev <sankaledev@gmail.com>
2024-02-07 12:53:24 +0300

    Make payment summary interactive.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -142,7 +142,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
 
-
+renderPaymentSummary();
   });
 });
 
@@ -153,6 +153,7 @@ element.addEventListener('click',()=>{
   const {productId,deliveryOptionId}= element.dataset
 updateDeliveryOption(productId,deliveryOptionId);
 renderOrderSummary();
+renderPaymentSummary();
 })
 });
 
